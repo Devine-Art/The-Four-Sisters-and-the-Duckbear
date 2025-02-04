d98e8c6e05337a024c3bf99605da9b0a036c6162
Devine-Art <adevine149@gmail.com>
2025-02-04 17:16:52 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Duckbear
+{ 
+    public class Program
+{
+    static void Main(string[] args)
+    {
+        //ask user for variables and assign them
+        Console.WriteLine("Enter # of chocolate eggs:");
+        int eggs = int.Parse(Console.ReadLine());
+
+
+        //Calculate how many eggs each should get
+        int eggsForSisters = eggs / 4;
+        int eggsForDuckbear = eggs % 4;
+
+        //write results to console
+        Console.WriteLine("Chocolate Eggs for each sister");
+        Console.WriteLine(eggsForSisters);
+        Console.WriteLine("Chocolate Eggs for duckbear");
+        Console.WriteLine(eggsForDuckbear);
+
+    }
+}
+}
